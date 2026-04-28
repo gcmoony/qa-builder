a96403f3073862d128a623e6c0d9eaedb291b2df
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-04-27 22:05:55 -0400

    Turn QAs into expanding content, add ability to add answer to QA

--- frontend/src/components/QA_List/ListItem.tsx
+++ frontend/src/components/QA_List/ListItem.tsx
@@ -1,11 +1,87 @@
+import { useState } from "react";
 import { Link } from "react-router";
+import { Alert } from "../Alerts/Alert";
+import { CollapseContent } from "../CollapseContent";
+
+const url = import.meta.env.VITE_DEV_MODE
+  ? import.meta.env.VITE_BACKEND_LOCAL
+  : import.meta.env.VITE_BACKEND_URL;
 
 export const ListItem = ({ id, question, answer }) => {
+  const [itemData, setItemData] = useState({
+    _id: id,
+    question: question,
+    answer: answer,
+  });
+
+  const [updatedData, setUpdatedData] = useState({
+    _id: id,
+    question: question,
+    answer: answer,
+  });
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const updateAnswer = async () => {
+    try {
+      const res = await fetch(`${url}/api/qas/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updatedData),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (res.ok) {
+        setItemData({ ...updatedData });
+      }
+    } catch (err) {
+      setTimeout(() => {
+        setError("");
+      }, 4000);
+      setError("Unable to update answer");
+      // setUpdatedData({ ...itemData });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = () => {
+    setError("");
+    setLoading(true);
+    updateAnswer();
+  };
+
   return (
-    <div className='card'>
-      <p className='question'>{question}</p>
-      <p className='answer'>{answer ? answer : "Pending answer..."}</p>
-      <Link to={`/qa/edit/${id}`}>Edit</Link>
-    </div>
+    <CollapseContent title={question}>
+      <div>
+        {itemData.answer ? (
+          <div className='flex justify-between'>
+            <span>{itemData.answer}</span>
+            <Link to={`/qa/edit/${id}`}>Edit Q&A</Link>
+          </div>
+        ) : (
+          <div className='flex flex-col '>
+            {loading && (
+              <span className='loading loading-dots loading-md'></span>
+            )}
+            {error && <Alert message={error} />}
+
+            <label htmlFor='answer_input'>Response</label>
+
+            <textarea
+              name='answer_input'
+              placeholder='Pending answer...'
+              value={updatedData.answer}
+              disabled={loading}
+              className={`resize-none textarea`}
+              onChange={(e) =>
+                setUpdatedData({ ...updatedData, answer: e.target.value })
+              }
+              onBlur={handleSubmit}
+            />
+          </div>
+        )}
+      </div>
+    </CollapseContent>
   );
 };
