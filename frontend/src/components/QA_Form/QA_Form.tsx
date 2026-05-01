48b627e394932a2c57d5256436a3e64dd89b69a5
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-05-01 12:53:03 -0400

    Updated form style

--- frontend/src/components/QA_Form/QA_Form.tsx
+++ frontend/src/components/QA_Form/QA_Form.tsx
@@ -72,47 +72,76 @@ export const QA_Form = () => {
 
   return (
     <div>
-      <div>
+      <div className='flex flex-col items-center p-5'>
+        <h2 className='text-3xl text-center my-5'>
+          {params.id ? "Edit Q&A" : "Submit a Q&A"}
+        </h2>
         <form
-          className='qa-form'
+          className='qa-form  w-full flex flex-col gap-5 '
           onSubmit={handleSubmit}
         >
-          <div className='input_component'>
+          <div className='input_component flex flex-col'>
             <label htmlFor='question_input'>Your Question</label>
 
             <textarea
               name='question_input'
+              className='resize-none outline textarea  w-full'
               value={qa_data.question}
               onChange={(e) =>
                 set_qa_data({ ...qa_data, question: e.target.value })
               }
             />
           </div>
-          <div className={`input_component ${params.id ? "show" : "hide"}`}>
+          <div
+            className={`input_component ${params.id ? "show" : "hide"}  flex flex-col `}
+          >
             <label htmlFor='answer_input'>Answer</label>
 
             <textarea
               name='answer_input'
               value={qa_data.answer}
+              className='resize-none outline textarea w-full'
               onChange={(e) =>
                 set_qa_data({ ...qa_data, answer: e.target.value })
               }
             />
           </div>
-          <button
-            type='submit'
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? (
-              <span>
-                {params.id ? "Updating Q&A..." : "Submitting Question..."}
-              </span>
-            ) : (
-              <span>{params.id ? "Edit Q&A" : "Submit Question"}</span>
-            )}
-          </button>
+          <div className='flex justify-end'>
+            <button
+              type='submit'
+              disabled={isSubmitting}
+              className='btn-primary btn'
+            >
+              {isSubmitting ? (
+                <span>
+                  {params.id ? "Updating Q&A..." : "Submitting Question..."}
+                </span>
+              ) : (
+                <span>{params.id ? "Edit Q&A" : "Submit Question"}</span>
+              )}
+            </button>
+          </div>
         </form>
-        <Link to={`/`}>Back to Q&As</Link>
+        <Link
+          to={`/`}
+          className='self-start flex gap-2 btn-link'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18'
+            />
+          </svg>
+          Back to Q&As
+        </Link>
       </div>
     </div>
   );
