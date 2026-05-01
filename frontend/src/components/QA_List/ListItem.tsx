6fa206e12516c9a9e723924845580d750dabfedf
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-05-01 11:44:40 -0400

    Add ability to delete QAs

--- frontend/src/components/QA_List/ListItem.tsx
+++ frontend/src/components/QA_List/ListItem.tsx
@@ -7,7 +7,7 @@ const url = import.meta.env.VITE_DEV_MODE
   ? import.meta.env.VITE_BACKEND_LOCAL
   : import.meta.env.VITE_BACKEND_URL;
 
-export const ListItem = ({ id, question, answer }) => {
+export const ListItem = ({ id, question, answer, deleteHandler }) => {
   const [itemData, setItemData] = useState({
     _id: id,
     question: question,
@@ -57,7 +57,34 @@ export const ListItem = ({ id, question, answer }) => {
         {itemData.answer ? (
           <div className='flex justify-between'>
             <span>{itemData.answer}</span>
-            <Link to={`/qa/edit/${id}`}>Edit Q&A</Link>
+            <div className='flex gap-2 flex-col'>
+              <Link
+                to={`/qa/edit/${id}`}
+                className='btn'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='size-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10'
+                  />
+                </svg>
+                Edit
+              </Link>
+              <button
+                className='btn text-error'
+                onClick={() => deleteHandler(id)}
+              >
+                Delete
+              </button>
+            </div>
           </div>
         ) : (
           <div className='flex flex-col '>
@@ -73,7 +100,7 @@ export const ListItem = ({ id, question, answer }) => {
               placeholder='Pending answer...'
               value={updatedData.answer}
               disabled={loading}
-              className={`resize-none textarea`}
+              className={`resize-none textarea w-full`}
               onChange={(e) =>
                 setUpdatedData({ ...updatedData, answer: e.target.value })
               }
--- frontend/src/components/QA_List/ListWrapper.tsx
+++ frontend/src/components/QA_List/ListWrapper.tsx
@@ -9,6 +9,24 @@ const url = import.meta.env.VITE_DEV_MODE
 export const ListWrapper = () => {
   const [qa_list, set_qa_list] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [toBeDeleted, setToBeDeleted] = useState("");
+  const modalElement = document.getElementById("confirm_delete");
+
+  const handleModalOpen = (itemId) => {
+    modalElement.showModal();
+    setToBeDeleted(itemId);
+  };
+
+  const handleDelete = async () => {
+    const result = await fetch(`${url}/api/qas/${toBeDeleted}`, {
+      method: "DELETE",
+    });
+    if (result.ok) {
+      setLoading(true);
+      setToBeDeleted("");
+      document.getElementById("confirm_delete").close();
+    }
+  };
 
   useEffect(() => {
     const loadList = async () => {
@@ -21,20 +39,99 @@ export const ListWrapper = () => {
     if (loading) {
       loadList();
     }
-  }, []);
+  }, [loading]);
 
   return (
     <div>
-      <Link to={"/qa/new"}>New Question</Link>
+      {/* Dialog */}
+      <dialog
+        id='confirm_delete'
+        className='modal'
+      >
+        <div className='modal-box'>
+          <div className='  flex flex-col text-center gap-5'>
+            <h3 className='text-2xl'>Confirm Delete</h3>
+            <p>Are you sure you want to delete this QA?</p>
+            <div className='modal-action'>
+              <form
+                method='dialog'
+                className='flex justify-between w-full'
+              >
+                <button className='btn btn-info'>Cancel</button>
+                <button
+                  className='btn hover:btn-error'
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleDelete();
+                  }}
+                >
+                  Delete QA
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </dialog>
+      {/* FAB */}
+      <div className='fab'>
+        <div
+          tabIndex={0}
+          role='button'
+          className='btn btn-lg btn-circle btn-primary'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M3.75 9h16.5m-16.5 6.75h16.5'
+            />
+          </svg>
+        </div>
+
+        <div>
+          New Question
+          <Link
+            className='btn btn-lg btn-circle'
+            to={"/qa/new"}
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='size-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M12 4.5v15m7.5-7.5h-15'
+              />
+            </svg>
+          </Link>
+        </div>
+      </div>
       <div>
-        {qa_list.map((item) => (
-          <ListItem
-            key={item._id}
-            id={item._id}
-            question={item.question}
-            answer={item.answer}
-          />
-        ))}
+        {qa_list.length > 0 ? (
+          qa_list.map((item) => (
+            <ListItem
+              key={item._id}
+              id={item._id}
+              question={item.question}
+              answer={item.answer}
+              deleteHandler={handleModalOpen}
+            />
+          ))
+        ) : (
+          <p className='text-center'>No Questions Found</p>
+        )}
       </div>
     </div>
   );
